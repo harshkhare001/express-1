3833e6979e1d76a48666bf17ce6f98ca411630aa
Harsh_Khare <72463880+harshkhare001@users.noreply.github.com>
2023-10-10 22:19:54 +0530

    middleware with body-parser

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use((req,res,next)=>{
-    console.log("in the middle");
-    next();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/addproducts',(req,res,next)=>{
+    //console.log("in the middle");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>')
+    //next();
 })
 
-app.use((req,res,next)=>{
-    console.log('in anouther middleware');
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
+    //console.log('in another middleware');
     res.send('<h1>Hello World from Express.Js<h1>')
 })
 
-const server = http.createServer(app);
-
-app.listen(4000);
+app.listen(3000);
